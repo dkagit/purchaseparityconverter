b03c1a8aad847be46c2c15dfb6309e7d6cf314f8
Karthick Devaraj <reachdkarthick@gmail.com>
2022-10-10 21:27:18 +0530

    Build Update

--- src/main/java/com/dkadev/purchaseparity/PurchaseparityApplication.java
+++ src/main/java/com/dkadev/purchaseparity/PurchaseparityApplication.java
@@ -2,9 +2,10 @@ package com.dkadev.purchaseparity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PurchaseparityApplication {
+public class PurchaseparityApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PurchaseparityApplication.class, args);
